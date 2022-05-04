fa6275785c17a250400b63fdcbd3464e27c19b9b
nisi0929 <kengo279@icloud.com>
2022-05-05 02:25:41 +0900

    env

--- src/axios/createInstance.ts
+++ src/axios/createInstance.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 export const CreateInstance = () => {
-  const baseURL = process.env.REACT_APP_DEV_API_URL;
+  const baseURL =
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_DEV_API_URL
+      : process.env.REACT_APP_PRODUCTION_API_URL;
   const client: AxiosInstance = axios.create({
     baseURL: baseURL,
     headers: { "Content-Type": "application/json" },
